2ba9c5c649c6e0843ab80334899ce3333c7c3510
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-12-14 21:17:55 -0600

    auth

--- src/components/signIn/Login.js
+++ src/components/signIn/Login.js
@@ -10,22 +10,70 @@ const Login =()=>{
     const[passwordError, setPasswordError]= useState('');
     const[hasAccount, setHasAccount]= useState('false');
 
+    const clearInputs = () =>{
+        setEmail("");
+        setPassword("");
+    }
+
+    const clearErrors = () =>{
+        setEmailError("");
+        setPasswordError("");
+    }
+
     const handleLogin = () => {
+        clearErrors();
         fire
             .auth()
             .signInWithEmailAndPassword(email, password)
+            .catch((err) => {
+                switch (err.code){
+                    case "auth/invalid-use":
+                    case "auth/user-disable":
+                    case "auth/user-not-found":
+                        setEmailError(err.message);
+                        break;
+                    case "auth/wrong-password":
+                        setPasswordError(err.message);
+                        break;  
+                }
+            })
+    }
+
+    const handleSigup = () =>{
+        clearErrors();
+        fire
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
             .catch((err) => {
                 switch (err.code){
                     case "auth/email-already-in-use":
                     case "auth/invalid-email":
                         setEmailError(err.message);
                         break;
-                    case "auth/weak-password":
-                        setPasswordError(err.message);
-                        break;  
+                     case "auth/weak-passwod":
+                         setPasswordError(err.message);
+                         break;   
                 }
             })
     }
+
+    const handleLogout = () =>{
+        fire.auth().sigOut();
+    }
+
+    const authListener = () =>{
+        fire.auth().onAuthStateChange((user) =>{
+            if(user){
+                setUser(user);
+            }else{
+                setUser("");
+            }
+        })
+    }
+
+    useEffect(() => {
+        authListener();
+    }, []);
     
     return(
         <div>
