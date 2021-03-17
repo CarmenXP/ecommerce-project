6f257202173c09952551707243956ebb02f0f671
CarmenXP <xocuaposadas.carmen@gmail.com>
2021-03-16 18:00:05 -0600

    actionsLogin

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+import{
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+    USER_SIGNIN_REQUEST,
+    USER_SIGNIN_SUCCESS,
+    USER_SIGNIN_FAIL,
+    USER_SIGNOUT
+} from '../constants/userConstants';
+
