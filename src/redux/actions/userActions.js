0686bb3d423b2cce12c75ae8df92118149653551
CarmenXP <xocuaposadas.carmen@gmail.com>
2021-03-16 22:04:58 -0600

    reducerslogin

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -10,3 +10,43 @@ import{
     USER_SIGNOUT
 } from '../constants/userConstants';
 
+export const register = (name, email, password) => async(dispatch)=>{
+    dispatch({type:USER_REGISTER_REQUEST, payload:{email, password}});
+    try{
+        const {data} = await axios.post('https://backendprojectecommerce.herokuapp.com/user', {
+            name,
+            email,
+            password
+        });
+        dispatch({type:USER_REGISTER_SUCCESS, payload: data.data});
+        dispatch({type:USER_SIGNIN_SUCCESS, payload: data.data});
+    }catch(error){
+        dispatch({type:USER_REGISTER_FAIL,
+        payload:
+            error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        });
+    }
+};
+
+export const signin = (email, password) => async(dispatch) => {
+    dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}});
+    try{
+        const {data} = await axios.post('https://backendprojectecommerce.herokuapp.com/auth', {email, password});
+        dispatch({type: USER_SIGNIN_SUCCESS, payload: data.data});
+    }catch(error){
+        dispatch({
+            type: USER_SIGNIN_FAIL,
+            payload:
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
+
+        });
+    }
+};
+
+export const signout = () => (dispatch) =>{
+    dispatch({type: USER_SIGNOUT});
+};
